fa287f34588088a0db0cb70405c52831c4bbf625
Vinnie <bhaskar.vinay1998@gmail.com>
2024-01-07 12:57:27 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,33 @@
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement(
+        "h1",
+        {
+          id: "heading",
+        },
+        "I am h1 tag!"
+      ),
+      React.createElement(
+        "h2",
+        {
+          id: "heading2",
+        },
+        "I am h2 tag!"
+      ),
+    ]
+  )
+);
+
+const heading = React.createElement("h1", {}, "Hello world form React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
