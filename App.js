9744a282d820f89e8000b99baa958e5b7d13e1b6
Vinnie <bhaskar.vinay1998@gmail.com>
2024-01-09 17:47:38 +0530

    ep2 p1

--- App.js
+++ App.js
@@ -1,35 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client" 
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent",
-  },
-  React.createElement(
-    "div",
-    {
-      id: "child",
-    },
-    [
-      React.createElement(
-        "h1",
-        {
-          id: "heading",
-        },
-        "I am h1 tag!"
-      ),
-      React.createElement(
-        "h2",
-        {
-          id: "heading2",
-        },
-        "I am h2 hsdiuewqihqidqduq!"
-      ),
-    ]
-  )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = (
+	<h1 className="heading" tabIndex="6">
+		Namaste React using JSX
+	</h1>
 );
-
-const heading = React.createElement("h1", {}, "Hello world form React!");
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
