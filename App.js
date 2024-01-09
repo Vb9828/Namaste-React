70ed0e2624faca1cd108230d0b8fc687e039a648
Vinnie <bhaskar.vinay1998@gmail.com>
2024-01-10 04:27:42 +0530

    Ep3 Final

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
+const HeadingComponent = () => (
+	<div id="container">
+		{Heading}
+		<h1> Namaste React functional component</h1>
+	</div>
+);
+const elem = <span>React Span </span>;
+const Heading = (
 	<h1 className="heading" tabIndex="6">
+		{elem}
 		Namaste React using JSX
 	</h1>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
