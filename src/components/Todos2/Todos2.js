83f9b53dd8f0e31d732045a85128a8a44960c7f3
Vinnie <bhaskar.vinay1998@gmail.com>
2024-03-17 02:24:34 +0530

    Todos2 change

--- src/components/Todos2/Todos2.js
+++ src/components/Todos2/Todos2.js
@@ -3,7 +3,8 @@ import TodoList from "./TodoList";
 
 const Todos2 = () => {
 	const [todoList, setTodoList] = useState(null);
-
+	const [todoObj, setTodoObj] = useState(null);
+	const [input, setInput] = useState("");
 	const fetchTodo = async () => {
 		const todos = await fetch("https://dummyjson.com/todos");
 		const json = await todos.json();
@@ -14,7 +15,23 @@ const Todos2 = () => {
 		const todos = await fetch("https://dummyjson.com/todos/" + id);
 		const json = await todos.json();
 		console.log("https://dummyjson.com/todos/" + id);
-		setTodoList(json);
+		setTodoObj(json);
+	};
+
+	const handleInput = (e) => {
+		setInput(e.target.value);
+	};
+
+	const addToList = () => {
+		var newItemObj = {};
+		newItemObj.id = todoList.length + 1;
+		newItemObj.todo = input;
+		newItemObj.userId = "22";
+		var newList = todoList.map((item) => item);
+		newList.push(newItemObj);
+		setTodoList(newList);
+		console.log("added");
+		setInput("");
 	};
 
 	useEffect(() => {
@@ -24,20 +41,31 @@ const Todos2 = () => {
 	return (
 		<div className="m-4 p-4 items-center">
 			<div className="m-4 p-4">
-				{todoList?.length > 0
-					? todoList?.map((item) => {
-							return (
-								<ul key={item.id}>
-									<li
-										onClick={() => {
-											fetchTodoById(item.id);
-										}}>
-										{item.todo}
-									</li>
-								</ul>
-							);
-					  })
-					: todoList?.todo}
+				<input
+					type="text"
+					className="m-4 p-4 border border-black shadow-lg"
+					value={input}
+					onChange={handleInput}
+				/>
+				<button
+					className="m-4 p-4 border border-black shadow-lg"
+					onClick={addToList}>
+					Add
+				</button>
+				<div>{todoObj != null ? todoObj.todo : ""}</div>
+				{todoList?.map((item) => {
+					return (
+						<ul key={item.id}>
+							<li
+								className="m-2 p-2 border border-gray-50 shadow-md w-2"
+								onClick={() => {
+									fetchTodoById(item.id);
+								}}>
+								{item.todo}
+							</li>
+						</ul>
+					);
+				})}
 			</div>
 		</div>
 	);
